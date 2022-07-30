50ef9ad684325926c2dec7360713149c353ab4dd
AdarshAIR1 <110090562+AdarshAIR1@users.noreply.github.com>
2022-07-30 22:33:56 +0530

    Add files via upload

---
+++ area.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    float b;
+    printf("Enter a number nupur don");
+    scanf("%d",&a);
+    b=(22/7)*a*a;
+    printf("Area of Circle is %f",b);
+    printf("/n");
+    return 0;
+
+}
